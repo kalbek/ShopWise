a43efe25229520d5eb629207c6b735082521e29a
kalbek <access2kaleb@gmail.com>
2023-05-10 15:30:47 +0300

    Setup cart items.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+
 function App() {
   return (
     <>
       <Navbar />
-      <h2>Redux Toolkit</h2>
+      <CartContainer />
     </>
   );
 }
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
 const initialState = {
   cartItems: cartItems,
-  amount: 0,
+  amount: cartItems.length,
   total: 0,
   isLoading: true,
 };
