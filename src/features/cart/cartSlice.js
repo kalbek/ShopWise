3888568264527084b242b8660731e2d337a8eb8c
kalbek <access2kaleb@gmail.com>
2023-05-10 15:54:10 +0300

    Add remove item reducer.

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
 const initialState = {
   cartItems: cartItems,
-  amount: cartItems.length,
+  amount: 4,
   total: 0,
   isLoading: true,
 };
@@ -14,8 +14,12 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
   },
 });
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem } = cartSlice.actions;
 console.log(cartSlice);
 export default cartSlice.reducer;
